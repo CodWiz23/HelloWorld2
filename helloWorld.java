af2ebad329e97fccf9e69d38363d88e053619c1e
Abdirahman Mohamed <abdirahman.mohamed@maine.edu>
2025-02-07 20:24:26 -0500

    Updated helloWorld.java with new changes

--- helloWorld.java
+++ helloWorld.java
@@ -3,5 +3,6 @@ public class helloWorld{
     System.out.println("Here ye : important announcement");
     System.out.println("Hello World!");
     System.out.println("Goodbye!");
+    System.out.println("Hi Alice");
   }
 }
